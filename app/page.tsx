591579fa6379e2399ea279c7230ced5bc7871f59
Calvin Li <calvin.xuan.li@gmail.com>
2023-08-28 21:45:16 -0700

    embed youtube video

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,49 @@
 'use client'
 import React from 'react';
-import { Image, Typography, ConfigProvider, theme, Row } from 'antd';
+import { Image, Typography, ConfigProvider, theme, Row, Tooltip } from 'antd';
 import { AiFillYoutube, AiOutlineInstagram, AiOutlineMail } from 'react-icons/ai'
 import { MdLocationPin } from 'react-icons/md'
-import { BsDiscord, BsTwitch } from 'react-icons/bs'
+import { BsDiscord } from 'react-icons/bs'
+import { BiLogoTwitch } from 'react-icons/bi'
 
 const primaryColor = 'white';
-const defaultSocialSize = '40px'
+const defaultSocialSize = '28px'
 const { Text, Title, Link } = Typography;
 const socials = [
+  {
+    'icon': <BiLogoTwitch />,
+    'link': 'https://www.twitch.tv/calvinxli',
+    'color': primaryColor,
+    'size': defaultSocialSize,
+    'ariaLabel': 'twitch.tv'
+  },
   {
     'icon': <AiOutlineInstagram />,
     'link': 'https://www.instagram.com/calvinxli',
     'color': primaryColor,
-    'size': defaultSocialSize
-  },
-  {
-    'icon': <BsTwitch />,
-    'link': 'https://www.twitch.tv/calvinxli',
-    'color': primaryColor,
-    'size': defaultSocialSize
+    'size': defaultSocialSize,
+    'ariaLabel': 'instagram.com'
   },
   {
     'icon': <AiFillYoutube />,
     'link': 'https://www.youtube.com/@calvinxli',
     'color': '#FF0000',
-    'size': '80px'
+    'size': '50px',
+    'ariaLabel': 'youtube.com'
   },
   {
     'icon': <BsDiscord />,
     'link': 'https://discord.gg/RD5wRyDEHE',
     'color': primaryColor,
-    'size': defaultSocialSize
+    'size': defaultSocialSize,
+    'ariaLabel': 'discord.gg'
   },
   {
     'icon': <AiOutlineMail />,
     'link': 'mailto:impaccengineer@gmail.com',
     'color': primaryColor,
     'size': defaultSocialSize,
+    'ariaLabel': 'email'
   }
 ]
 
@@ -55,18 +61,29 @@ const HomePage = () => (
       <Image width={150} src='/profile_pic.jpg' alt="Calvin's profile picture" preview={false} style={{ borderRadius: '50%', border: '2px solid white' }} />
     </Row>
     <Row justify='center' align='middle'>
-      <Title style={{ margin: 12 }}>
-        Calvin Li
-      </Title>
+      <Tooltip title='Calvin Li'>
+        <Title aria-label='Calvin Li' level={3} style={{ margin: 10, color: 'white' }}>
+          @calvinxli
+        </Title>
+      </Tooltip>
     </Row>
     <Row justify='center' align='middle'>
-      <Title level={4} style={{ margin: 0 }}><MdLocationPin />Seattle, WA</Title>
+      <Title level={5} style={{ margin: 0 }}><MdLocationPin />Seattle, WA</Title>
     </Row>
     <Row justify='center' align='middle'>
       <Title level={5} style={{ margin: 0 }}>Tech | Foodie | Lifestyle</Title>
     </Row>
     <Row justify='center' align='middle'>
-      {socials.map((social, _index) => <Link key={_index} style={{ "fontSize": social.size, "color": social.color }} href={social.link}>{social.icon}</Link>)}
+      {socials.map((social, _index) => <Link key={_index} aria-label={social.ariaLabel} style={{ padding: "8px", fontSize: social.size, "color": social.color }} href={social.link}>{social.icon}</Link>)}
+    </Row>
+    <Row justify='center' align='middle'>
+      <Title aria-label='Calvin Li' level={3} style={{ margin: 10, color: 'white' }}>
+        Latest Vlog
+      </Title>
+    </Row>
+    <Row justify='center' align='middle'>
+      <iframe id="ytplayer" itemType='text/html' width="100%" style={{ borderRadius: '5%' }}
+        src="https://www.youtube.com/embed/hwsGB-XObvA" frameBorder={0} allowFullScreen></iframe>
     </Row>
   </ConfigProvider>
 );
