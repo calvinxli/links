3e80bbb52bb35443c6fe427a66f53f640fadcd76
Calvin Li <calvin.xuan.li@gmail.com>
2023-08-29 00:09:01 -0700

    refactor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google';
-import StyledComponentsRegistry from '../lib/AntdRegistry';
-import '@/app/globals.css';
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import StyledComponentsRegistry from "../lib/AntdRegistry";
+import "@/app/globals.css";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Calvin Li\'s Personal Links Website',
-  description: 'Contains links for Calvin\'s social media, affiliates and more',
-}
+  title: "Calvin Li's Personal Links Website",
+  description: "Contains links for Calvin's social media, affiliates and more",
+};
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang="en">
--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,17 @@
-'use client'
-import React from 'react';
-import { Image, Typography, ConfigProvider, theme, Row, Tooltip } from 'antd';
-import { AiFillYoutube, AiOutlineInstagram, AiOutlineMail } from 'react-icons/ai'
-import { MdLocationPin } from 'react-icons/md'
-import { BsDiscord } from 'react-icons/bs'
-import { BiLogoTwitch } from 'react-icons/bi'
+"use client";
+import React from "react";
+import { Image, Typography, ConfigProvider, theme, Row, Tooltip } from "antd";
+import {
+  AiFillYoutube,
+  AiOutlineInstagram,
+  AiOutlineMail,
+} from "react-icons/ai";
+import { MdLocationPin } from "react-icons/md";
+import { BsDiscord } from "react-icons/bs";
+import { BiLogoTwitch } from "react-icons/bi";
+import { SocialLinkButton } from "@/components/Social";
 
-const primaryColor = 'white';
-const defaultSocialSize = '28px'
-const { Text, Title, Link } = Typography;
-const socials = [
-  {
-    'icon': <BiLogoTwitch />,
-    'link': 'https://www.twitch.tv/calvinxli',
-    'color': primaryColor,
-    'size': defaultSocialSize,
-    'ariaLabel': 'twitch.tv'
-  },
-  {
-    'icon': <AiOutlineInstagram />,
-    'link': 'https://www.instagram.com/calvinxli',
-    'color': primaryColor,
-    'size': defaultSocialSize,
-    'ariaLabel': 'instagram.com'
-  },
-  {
-    'icon': <AiFillYoutube />,
-    'link': 'https://www.youtube.com/@calvinxli',
-    'color': '#FF0000',
-    'size': '50px',
-    'ariaLabel': 'youtube.com'
-  },
-  {
-    'icon': <BsDiscord />,
-    'link': 'https://discord.gg/RD5wRyDEHE',
-    'color': primaryColor,
-    'size': defaultSocialSize,
-    'ariaLabel': 'discord.gg'
-  },
-  {
-    'icon': <AiOutlineMail />,
-    'link': 'mailto:impaccengineer@gmail.com',
-    'color': primaryColor,
-    'size': defaultSocialSize,
-    'ariaLabel': 'email'
-  }
-]
+const { Text, Title } = Typography;
 
 const HomePage = () => (
   <ConfigProvider
@@ -57,33 +23,88 @@ const HomePage = () => (
       // algorithm: [theme.darkAlgorithm, theme.compactAlgorithm],
     }}
   >
-    <Row justify='center' align='middle'>
-      <Image width={150} src='/profile_pic.jpg' alt="Calvin's profile picture" preview={false} style={{ borderRadius: '50%', border: '2px solid white' }} />
+    <Row justify="center" align="middle">
+      <Image
+        width={150}
+        src="/profile_pic.jpg"
+        alt="Calvin's profile picture"
+        preview={false}
+        style={{ borderRadius: "50%", border: "2px solid white" }}
+      />
     </Row>
-    <Row justify='center' align='middle'>
-      <Tooltip title='Calvin Li'>
-        <Title aria-label='Calvin Li' level={3} style={{ margin: 10, color: 'white' }}>
+    <Row justify="center" align="middle">
+      <Tooltip title="Calvin Li">
+        <Title
+          aria-label="Calvin Li"
+          level={3}
+          style={{ margin: 10, color: "white" }}
+        >
           @calvinxli
         </Title>
       </Tooltip>
     </Row>
-    <Row justify='center' align='middle'>
-      <Title level={5} style={{ margin: 0 }}><MdLocationPin />Seattle, WA</Title>
+    <Row justify="center" align="middle">
+      <Title level={5} style={{ margin: 0 }}>
+        <MdLocationPin />
+        Seattle, WA
+      </Title>
     </Row>
-    <Row justify='center' align='middle'>
-      <Title level={5} style={{ margin: 0 }}>Tech | Foodie | Lifestyle</Title>
+    <Row justify="center" align="middle">
+      <Title level={5} style={{ margin: 0 }}>
+        Tech | Foodie | Lifestyle
+      </Title>
     </Row>
-    <Row justify='center' align='middle'>
-      {socials.map((social, _index) => <Link key={_index} aria-label={social.ariaLabel} style={{ padding: "8px", fontSize: social.size, "color": social.color }} href={social.link}>{social.icon}</Link>)}
+    <Row justify="center" align="middle">
+      <SocialLinkButton
+        icon={<BiLogoTwitch />}
+        link="https://www.twitch.tv/calvinxli"
+        ariaLabel="twitch.tv"
+      />
+      <SocialLinkButton
+        icon={<AiOutlineInstagram />}
+        link="https://www.instagram.com/calvinxli"
+        ariaLabel="instagram.com"
+      />
+      <SocialLinkButton
+        icon={<AiFillYoutube />}
+        link="https://www.youtube.com/@calvinxli"
+        ariaLabel="youtube.com"
+        color="#FF0000"
+        size="50px"
+      />
+      <SocialLinkButton
+        icon={<BsDiscord />}
+        link="https://discord.gg/RD5wRyDEHE"
+        ariaLabel="discord.gg"
+      />
+      <SocialLinkButton
+        icon={<AiOutlineMail />}
+        link="mailto:impaccengineer@gmail.com"
+        ariaLabel="email"
+      />
     </Row>
-    <Row justify='center' align='middle'>
-      <Title aria-label='Calvin Li' level={3} style={{ margin: 10, color: 'white' }}>
+    <Row justify="center" align="middle">
+      <Title
+        aria-label="Calvin Li"
+        level={3}
+        style={{ margin: 10, color: "white" }}
+      >
         Latest Vlog
       </Title>
     </Row>
-    <Row justify='center' align='middle'>
-      <iframe id="ytplayer" itemType='text/html' max-width="640px" style={{ borderRadius: '5%' }}
-        src="https://www.youtube.com/embed/hwsGB-XObvA" frameBorder={0} allowFullScreen></iframe>
+    <Row justify="center" align="middle">
+      <iframe
+        id="ytplayer"
+        itemType="text/html"
+        style={{
+          borderRadius: "5%",
+          aspectRatio: "16/9",
+          width: "100%",
+          border: 0,
+        }}
+        src="https://www.youtube.com/embed/hwsGB-XObvA"
+        allowFullScreen
+      ></iframe>
     </Row>
   </ConfigProvider>
 );
