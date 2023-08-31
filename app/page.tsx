820df69e5036615eb79951a2799fa8ac5663beb7
Calvin Li <calvin.xuan.li@gmail.com>
2023-08-30 23:16:04 -0700

    stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Image, Typography, ConfigProvider, theme, Row, Tooltip } from "antd";
+import { Image, Typography, Row, Tooltip, Collapse } from "antd";
 import {
   AiFillYoutube,
   AiOutlineInstagram,
@@ -11,6 +11,7 @@ import { BsDiscord } from "react-icons/bs";
 import { BiLogoTwitch } from "react-icons/bi";
 import { SocialLinkButton } from "@/components/Social";
 import { PageLayout } from "@/components/PageLayout";
+import LinkList from "@/components/LinkList";
 
 const { Text, Title } = Typography;
 
@@ -78,6 +79,16 @@ const HomePage = () => (
             ariaLabel="email"
           />
         </Row>
+        {/* <LinkList
+          header="Affiliates"
+          items={[
+            {
+              title: "Hello",
+              subtitle: "world",
+              link: "https://www.google.com/",
+            },
+          ]}
+        /> */}
         <Row justify="center" align="middle">
           <Title
             aria-label="Calvin Li"
@@ -104,100 +115,6 @@ const HomePage = () => (
       </>
     }
   />
-
-  // <ConfigProvider
-  //   theme={{
-  //     // 1. Use dark algorithm
-  //     algorithm: theme.darkAlgorithm,
-
-  //     // 2. Combine dark algorithm and compact algorithm
-  //     // algorithm: [theme.darkAlgorithm, theme.compactAlgorithm],
-  //   }}
-  // >
-  //   <Row justify="center" align="middle">
-  //     <Image
-  //       width={150}
-  //       src="/profile_pic.jpg"
-  //       alt="Calvin's profile picture"
-  //       preview={false}
-  //       style={{ borderRadius: "50%", border: "2px solid white" }}
-  //     />
-  //   </Row>
-  //   <Row justify="center" align="middle">
-  //     <Tooltip title="Calvin Li">
-  //       <Title
-  //         aria-label="Calvin Li"
-  //         level={3}
-  //         style={{ margin: 10, color: "white" }}
-  //       >
-  //         @calvinxli
-  //       </Title>
-  //     </Tooltip>
-  //   </Row>
-  //   <Row justify="center" align="middle">
-  //     <Title level={5} style={{ margin: 0 }}>
-  //       <MdLocationPin />
-  //       Seattle, WA
-  //     </Title>
-  //   </Row>
-  //   <Row justify="center" align="middle">
-  //     <Title level={5} style={{ margin: 0 }}>
-  //       Tech | Foodie | Lifestyle
-  //     </Title>
-  //   </Row>
-  //   <Row justify="center" align="middle">
-  //     <SocialLinkButton
-  //       icon={<BiLogoTwitch />}
-  //       link="https://www.twitch.tv/calvinxli"
-  //       ariaLabel="twitch.tv"
-  //     />
-  //     <SocialLinkButton
-  //       icon={<AiOutlineInstagram />}
-  //       link="https://www.instagram.com/calvinxli"
-  //       ariaLabel="instagram.com"
-  //     />
-  //     <SocialLinkButton
-  //       icon={<AiFillYoutube />}
-  //       link="https://www.youtube.com/@calvinxli"
-  //       ariaLabel="youtube.com"
-  //       color="#FF0000"
-  //       size="50px"
-  //     />
-  //     <SocialLinkButton
-  //       icon={<BsDiscord />}
-  //       link="https://discord.gg/RD5wRyDEHE"
-  //       ariaLabel="discord.gg"
-  //     />
-  //     <SocialLinkButton
-  //       icon={<AiOutlineMail />}
-  //       link="mailto:impaccengineer@gmail.com"
-  //       ariaLabel="email"
-  //     />
-  //   </Row>
-  //   <Row justify="center" align="middle">
-  //     <Title
-  //       aria-label="Calvin Li"
-  //       level={3}
-  //       style={{ margin: 10, color: "white" }}
-  //     >
-  //       Latest Vlog
-  //     </Title>
-  //   </Row>
-  //   <Row justify="center" align="middle">
-  //     <iframe
-  //       id="ytplayer"
-  //       itemType="text/html"
-  //       style={{
-  //         borderRadius: "5%",
-  //         aspectRatio: "16/9",
-  //         width: "100%",
-  //         border: 0,
-  //       }}
-  //       src="https://www.youtube.com/embed/hwsGB-XObvA"
-  //       allowFullScreen
-  //     ></iframe>
-  //   </Row>
-  // </ConfigProvider>
 );
 
 export default HomePage;
--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -1,5 +1,5 @@
 import { Layout, Menu, Typography, theme, ConfigProvider } from "antd";
-import { FunctionComponent } from "react";
+import React from "react";
 
 const { Text, Title } = Typography;
 const { Header, Content, Footer } = Layout;
@@ -11,55 +11,72 @@ type PageLayoutProps = {
 const headerPadding = "16px";
 const name = "Calvin Li";
 const home = "Home";
+const products = "Products/Gear";
 
-const navLabels = [
-  home,
-  // "Products/Gear"
-];
+export class PageLayout extends React.Component<PageLayoutProps> {
+  constructor(props: PageLayoutProps) {
+    super(props);
+  }
 
-export const PageLayout: FunctionComponent<PageLayoutProps> = ({ content }) => (
-  <ConfigProvider
-    theme={{
-      // 1. Use dark algorithm
-      algorithm: theme.darkAlgorithm,
+  state = {
+    current: home,
+  };
 
-      // 2. Combine dark algorithm and compact algorithm
-      // algorithm: [theme.darkAlgorithm, theme.compactAlgorithm],
-    }}
-  >
-    <Layout className="layout">
-      <Header
-        style={{
-          display: "flex",
-          alignItems: "center",
-          marginBottom: headerPadding,
+  // TODO make the page not reload
+  handleClick = (e: { key: string }) => {
+    this.setState({
+      current: e.key,
+    });
+  };
+
+  render() {
+    return (
+      <ConfigProvider
+        theme={{
+          // 1. Use dark algorithm
+          algorithm: theme.darkAlgorithm,
+
+          // 2. Combine dark algorithm and compact algorithm
+          // algorithm: [theme.darkAlgorithm, theme.compactAlgorithm],
         }}
       >
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          style={{ width: "100%" }}
-          defaultSelectedKeys={[home]}
-          items={navLabels.map((key, _index) => {
-            return {
-              key,
-              label: key,
-            };
-          })}
-        />
-        <Title
-          level={4}
-          style={{
-            margin: headerPadding,
-            marginRight: 0,
-            whiteSpace: "nowrap",
-          }}
-        >
-          {name}
-        </Title>
-      </Header>
-      <Content style={{ padding: "0 50px" }}>{content}</Content>
-      {/* <Footer style={{ textAlign: "center" }}>Calvin Li</Footer> */}
-    </Layout>
-  </ConfigProvider>
-);
+        <Layout className="layout">
+          <Header
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: headerPadding,
+            }}
+          >
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              style={{ width: "100%" }}
+              onClick={this.handleClick}
+              selectedKeys={[this.state.current]}
+            >
+              <Menu.Item key={home}>
+                <a href="/">{home}</a>
+              </Menu.Item>
+              {/* <Menu.Item key={products}>
+                <a href="/products">{products}</a>
+              </Menu.Item> */}
+            </Menu>
+            <Title
+              level={4}
+              style={{
+                margin: headerPadding,
+                marginRight: 0,
+                whiteSpace: "nowrap",
+              }}
+            >
+              {name}
+            </Title>
+          </Header>
+          <Content style={{ padding: "0 50px" }}>{this.props.content}</Content>
+          {/* <Footer style={{ textAlign: "center" }}>Calvin Li</Footer> */}
+        </Layout>
+      </ConfigProvider>
+    );
+  }
+}
