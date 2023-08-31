e4cb8fa736cb721346754c86fd6cb51e9ccd1d9a
Calvin Li <calvin.xuan.li@gmail.com>
2023-08-30 20:31:04 -0700

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,36 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import StyledComponentsRegistry from "../lib/AntdRegistry";
 import "@/app/globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
+const author = "Calvin Li";
 
 export const metadata: Metadata = {
-  title: "Calvin Li's Personal Links Website",
+  applicationName: author,
+  title: author,
   description: "Contains links for Calvin's social media, affiliates and more",
+  authors: [{ name: author, url: "https://calvinxli.com" }],
+  creator: author,
+  themeColor: { media: "(prefers-color-scheme: dark)", color: "#000000" },
+  colorScheme: "dark",
+  robots: "index, folloow",
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => (
   <html lang="en">
+    {/* <Head> */}
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-TEL22919JK" />
+    <Script id="google-analytics">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-TEL22919JK');
+        `}
+    </Script>
+    {/* </Head> */}
     <body className={inter.className}>
       <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
     </body>
